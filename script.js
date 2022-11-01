d88daaa2dac64bb493cb47b67b9bab82a7560058
LedBajrami <108076169+LedBajrami@users.noreply.github.com>
2022-11-01 17:24:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+const hiddenElements2 = document.querySelectorAll('.hidden2')
+const hiddenElements3 = document.querySelectorAll('.hidden3')
+hiddenElements.forEach((el) => observer.observe(el))
+hiddenElements2.forEach((el) => observer.observe(el))
+hiddenElements3.forEach((el) => observer.observe(el))
